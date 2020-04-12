0a87e9e266a61a81087a725f6c05264262d9e0b4
veljkoza <veljko.sekulic1998@gmail.com>
2020-04-12 23:54:44 +0200

    travelers number function partially added

--- scripts/search-flights.js
+++ scripts/search-flights.js
@@ -9,8 +9,11 @@ const travelersAllOptions = $(".travelers");
 const cabinAllOptions = $(".cabin");
 
 const retDate = $(".returnDate");
-console.log(retDate)
 const searchOptions = document.querySelectorAll(".col-ul");
+let adults = 1;
+let children = 0;
+let infants = 0;
+let travelers = 1;
 
 function $(selector) {
   return document.querySelector(selector);
@@ -62,12 +65,12 @@ function $(selector) {
 
     [...tripUl.children].forEach((li) => {
       li.addEventListener("click", (e) => {
-        if(li.textContent !== "Round trip"){
-          formContainer.classList.add("one-way")
-        }else{
+        if (li.textContent !== "Round trip") {
+          formContainer.classList.add("one-way");
+        } else {
           formContainer.classList.remove("one-way");
         }
-        switchTextContent(li,tripUl);
+        switchTextContent(li, tripUl);
         // tripUl.parentElement.classList.add("hide");
         // let icon = tripOption.children[0];
         // tripOption.textContent = li.textContent+" ";
@@ -75,25 +78,100 @@ function $(selector) {
       });
     });
 
-    [...cabinUl.children].forEach(li=>{
-      li.addEventListener("click",e=>{
-        switchTextContent(li,cabinUl);
-      })
-    })
+    [...cabinUl.children].forEach((li) => {
+      li.addEventListener("click", (e) => {
+        switchTextContent(li, cabinUl);
+      });
+    });
 
-    function switchTextContent(li,ul){
+    let adultsSpan = $("#noOfAdults");
+    let childrenSpan = $("#noOfChildren");
+    let noOfInfantsSpan = $("#noOfInfants");
+
+    [...travelersUl.children].forEach((li) => {
+      let colRightElements = li.lastElementChild.children;
+      let minusBtn = colRightElements[0];
+      let noOfPeopleSpan = colRightElements[1];
+      let plusBtn = colRightElements[2];
+      switch (noOfPeopleSpan) {
+        case adultsSpan:
+          plusMinusEventListenersFor("adults", adultsSpan);
+          break;
+        case childrenSpan:
+          plusMinusEventListenersFor("children", childrenSpan);
+          console.log("test2");
+
+          break;
+        case noOfInfantsSpan:
+          console.log("test3");
+          plusMinusEventListenersFor("infants", noOfInfantsSpan);
+          break;
+      }
+
+      function plusMinusEventListenersFor(no, spanElement) {
+        minusBtn.addEventListener("click", (e) => {
+          e.stopPropagation();
+          if (no <= 0) {
+            return;
+          } else {
+            no--;
+            travelers--;
+          }
+          changeTravelersText();
+          console.log(
+            `adults: ${adults},children: ${children},infants${infants}`
+          );
+          spanElement.textContent = no;
+        });
+
+        plusBtn.addEventListener("click", (e) => {
+          e.stopPropagation();
+          no++;
+          travelers++;
+          changeTravelersText();
+          console.log(
+            `adults: ${adults},children: ${children},infants${infants}`
+          );
+
+          spanElement.textContent = no;
+        });
+
+        function changeTravelersText() {
+          let whoTravels = checkIfTravelers();
+          let icon = createIcon();
+          travelersOption.textContent = `${travelers} ${whoTravels}`;
+          travelersOption.appendChild(icon);
+        }
+      }
+
+      function checkIfTravelers() {
+        if (adults > 0 && children === 0 && infants === 0) {
+          console.log("test2");
+
+          return "adults";
+        } else if (adults === 0 && children > 0 && infants === 0) {
+          return "children";
+        } else if (adults === 0 && children === 0 && infants > 2) {
+          alert("Infants can't go on plane without adult's supervision!");
+          return "adults";
+        } else {
+          return "travelers";
+        }
+      }
+    });
+
+    function switchTextContent(li, ul) {
       ul.parentElement.classList.add("hide");
 
       let optionForChange = ul.parentElement.parentElement.children[0];
-      optionForChange.textContent = li.textContent+" ";
+      optionForChange.textContent = li.textContent + " ";
       let icon = createIcon();
       optionForChange.appendChild(icon);
-
     }
 
-    function createIcon(){
+    function createIcon() {
       let icon = document.createElement("i");
-      icon.classList.add("fas","fa-chevron-down");
+      icon.classList.add("fas", "fa-chevron-down");
       return icon;
     }
   })();
