de8871b001e3251b9e920009970ea0f5602fe3f8
rad1na <radinovicpetar98@gmail.com>
2020-04-13 22:47:31 +0200

    Almost finished about,needs some fixes

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,12 +1,14 @@
 (function (){
     const moreInfoButtons = document.querySelectorAll(".pop-up-button");
-    [...moreInfoButtons].forEach(button => button.addEventListener("click",moreInfoPop));
+    [...moreInfoButtons].forEach(button => button.addEventListener("click",moreInfo));
     const aboutExitButton = document.querySelector(".about-exit").addEventListener("click",moreInfoClose);
 })()
 
-function moreInfoPop(e){
-    const aboutWindow = document.querySelector(".about-us-window");
-    aboutWindow.style.display = "flex";
+function moreInfo(e){
+    //const aboutWindow = document.querySelector(".about-us-window");
+    //aboutWindow.style.display = "flex";
+    //aboutWindow.style.opacity = "100";
+    console.log(this);
 }
 
 function moreInfoClose(e){
