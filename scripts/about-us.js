16108f8eb6ad36258685b279fa2a10f10d4d6d95
rad1na <radinovicpetar98@gmail.com>
2020-04-15 23:32:28 +0200

    Final fixes to about-us

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,8 +1,11 @@
 (function (){
     const moreInfoButtons = document.querySelectorAll(".pop-up-button");
     const closeInfoButtons = document.querySelectorAll("#about-close-button");
+    const allBoxes = document.querySelectorAll(".about-box");
     [...closeInfoButtons].forEach(button => button.addEventListener("click",moreInfo));
     [...moreInfoButtons].forEach(button => {if(button.id !== "about-close-button")button.addEventListener("click",moreInfo)});
+    [...allBoxes].forEach(box => box.addEventListener("animationend",deleteAnimations));
+
 })()
 
 function moreInfo(e){
@@ -19,5 +22,9 @@ function moreInfo(e){
 function handleMouseOut(e){
     this.parentNode.style.transform = "rotateY(0deg)";
     this.removeEventListener("mouseleave",handleMouseOut);
+}
+
+function deleteAnimations(){
+    this.classList.remove("animated","fadeInLeft","fadeInUp","fadeInRight","about-delay-03","about-delay-06");
 }
 
